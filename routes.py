210c2897fc7fafd1333c653def26bf3ffd6d4e63
Andrey "zziikkemd" Zykov <zziikkemd@gmail.com>
2020-05-10 17:10:37 +0300

    Moved routes class to separate file

---
+++ routes.py
@@ -0,0 +1,73 @@
+from aiohttp import web
+
+
+class Routes:
+    def __init__(self):
+        self.routes = [
+            web.get('/add_entry', self.add_entry),
+            web.get('/remove_day', self.remove_day),
+            web.get('/add_raw_data', self.add_raw_data)
+        ]
+
+    async def add_entry(self, request):
+        data = request.query
+        db = request.app['db']
+
+        if 'email' in data:
+            email = data['email']
+        else:
+            return web.Response(text='В запросе не указана почта')
+
+        if 'time' in data:
+            time = data['time']
+        else:
+            return web.Response(text='В запросе не указано время отправки')
+
+        try:
+            await db.add_entry(email, time)
+        except Exception as e:
+            return web.Response(text='Ошибка: {}'.format(str(e)))
+
+        return web.Response(text='Адрес поставлен в очередь')
+
+    async def remove_day(self, request):
+        data = request.query
+        db = request.app['db']
+
+        if 'day' in data:
+            day = data['day']
+        else:
+            return web.Response(text='В запросе не указан день очистки')
+
+        try:
+            await db.clear_day(day)
+        except Exception as e:
+            return web.Response(text='Ошибка: {}'.format(str(e)))
+
+        return web.Response(text='День очищен от запросов')
+
+    async def add_raw_data(self, request):
+        data = request.query
+        db = request.app['db']
+
+        if 'name' in data:
+            name = data['name']
+        else:
+            return web.Response(text='В запросе не указано имя')
+
+        if 'email' in data:
+            email = data['email']
+        else:
+            return web.Response(text='В запросе не указана почта')
+
+        if 'phone' in data:
+            phone = data['phone']
+        else:
+            return web.Response(text='В запросе не указан телефон')
+
+        try:
+            await db.add_raw_entry(name, email, phone)
+        except Exception as e:
+            return web.Response(text='Ошибка: {}'.format(str(e)))
+
+        return web.Response(text='Данные записаны')
--- server.py
+++ server.py
@@ -6,72 +6,8 @@ from dateutil.tz import gettz
 
 from logger import log_error
 from postgres import DataBase
+from routes import Routes
 from unisender import Api
-
-
-class RoutesHandlers:
-    async def add_entry(self, request):
-        data = request.query
-        db = request.app['db']
-
-        if 'email' in data:
-            email = data['email']
-        else:
-            return web.Response(text='В запросе не указана почта')
-
-        if 'time' in data:
-            time = data['time']
-        else:
-            return web.Response(text='В запросе не указано время отправки')
-
-        try:
-            await db.add_entry(email, time)
-        except Exception as e:
-            return web.Response(text='Ошибка: {}'.format(str(e)))
-
-        return web.Response(text='Адрес поставлен в очередь')
-
-    async def remove_day(self, request):
-        data = request.query
-        db = request.app['db']
-
-        if 'day' in data:
-            day = data['day']
-        else:
-            return web.Response(text='В запросе не указан день очистки')
-
-        try:
-            await db.clear_day(day)
-        except Exception as e:
-            return web.Response(text='Ошибка: {}'.format(str(e)))
-
-        return web.Response(text='День очищен от запросов')
-
-    async def add_raw_data(self, request):
-        data = request.query
-        db = request.app['db']
-
-        if 'name' in data:
-            name = data['name']
-        else:
-            return web.Response(text='В запросе не указано имя')
-
-        if 'email' in data:
-            email = data['email']
-        else:
-            return web.Response(text='В запросе не указана почта')
-
-        if 'phone' in data:
-            phone = data['phone']
-        else:
-            return web.Response(text='В запросе не указан телефон')
-
-        try:
-            await db.add_raw_entry(name, email, phone)
-        except Exception as e:
-            return web.Response(text='Ошибка: {}'.format(str(e)))
-
-        return web.Response(text='Данные записаны')
 
 
 class Server():
@@ -84,11 +20,8 @@ class Server():
             dbConfig['dbname'], dbConfig['tinname'], dbConfig['toutname'])
         self.app['api'] = Api(apiConfig['key'], apiConfig['list'])
 
-        handlers = RoutesHandlers()
-        self.app.add_routes(
-            [web.get('/add_entry', handlers.add_entry),
-             web.get('/remove_day', handlers.remove_day),
-             web.get('/add_raw_data', handlers.add_raw_data)])
+        handlers = Routes()
+        self.app.add_routes(handlers.routes)
 
     def start(self):
         asyncio.get_event_loop().run_until_complete(
@@ -123,7 +56,7 @@ class Server():
         async def cleanup_background_tasks(app):
             app['sender'].cancel()
 
-        self.app.on_startup.append(start_background_tasks)
+        #self.app.on_startup.append(start_background_tasks)
         self.app.on_cleanup.append(cleanup_background_tasks)
 
         web.run_app(self.app)
