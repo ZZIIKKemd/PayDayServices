dab01e8ed529a5717f4737d24e7e5513f1a40c8d
Andrey "zziikkemd" Zykov <zziikkemd@gmail.com>
2020-12-24 01:01:16 +0300

    Refactored starter code to OOP-style

--- PayDayService/start.py
+++ PayDayService/start.py
@@ -6,40 +6,43 @@ from api.factory import ApiFactory
 from common.config import Config
 from common.logger import Logger
 
-LOG = '.log'
-CONFIG = 'config.yml'
 
+class Starter:
+    logfile = '.log'
+    configfile = 'config.yml'
 
-def __error__():
-    print('Ошибка при инициализации. Ознакомьтесь с файлом {}'.format(LOG))
-    exit(0)
+    def __init__(self):
+        self.logger = Logger(self.logfile)
+        self.logger.info('Лог-файл "{}" инициализирован'.format(self.logfile))
 
-
-def main():
-    logger = Logger(LOG)
-    logger.info('Лог-файл "{}" инициализирован'.format(LOG))
-
-    try:
-        config = Config(CONFIG)
-    except Exception as e:
-        logger.critical(e)
-        __error__()
-    logger.info('Файл конфигурации загружен')
-
-    apis = ApiCollection()
-    for apiname, apiconfig in config['apis'].items():
         try:
-            apis[apiname] = ApiFactory.new(apiname, apiconfig)
+            config = Config(self.configfile)
         except Exception as e:
-            logger.critical(e)
-            __error__()
-        s = 'API с идентификатором "{}" инициализирован'.format(apiname)
-        logger.info(s)
-    logger.info('Все API инициализированы')
+            self._init_error(e)
+        self.logger.info('Файл конфигурации загружен')
 
-    app = apis['goip']
-    print(app._name)
+        apis = ApiCollection()
+        for apiname, apiconfig in config['apis'].items():
+            try:
+                apis[apiname] = ApiFactory.new(apiname, apiconfig)
+            except Exception as e:
+                self._init_error(e)
+            s = 'API с идентификатором "{}" инициализирован'.format(apiname)
+            self.logger.info(s)
+        self.logger.info('Все API инициализированы')
+
+        app = apis['goip']
+        print(app._name)
+
+    def _init_error(self, error: Exception):
+        """Logs critical error and closes the program with helpful message
+        """        
+        self.logger.critical(error)
+        s = 'Ошибка при инициализации. Ознакомьтесь с файлом {}'
+        s = s.format(self.logfile)
+        print()
+        exit(0)
 
 
 if __name__ == "__main__":
-    main()
+    prog = Starter()
