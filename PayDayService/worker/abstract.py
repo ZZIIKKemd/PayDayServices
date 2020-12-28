1d7633d7f2a8e36568e9c246164d866921d61e78
Andrey "zziikkemd" Zykov <zziikkemd@gmail.com>
2020-12-28 22:30:49 +0300

    Added abstract workers classes

---
+++ PayDayService/worker/abstract.py
@@ -0,0 +1,72 @@
+from typing import Dict
+
+from common.exception import ServiceException
+
+
+class WorkerConfigurationException(ServiceException):
+    pass
+
+
+class WorkerException(ServiceException):
+    pass
+
+
+class Worker:
+    _type = ''
+
+    def __init__(self, name: str, config: Dict):
+        """Abstract worker class with basic initialization
+        """
+        self._name = name
+        self._config = config
+
+    def get_name(self) -> str:
+        """Returns worker identifier
+        """
+        return self._name
+
+    def get_type(self) -> str:
+        """ Returns worker type
+        """
+        return self._type
+
+    def _is_correct_field(self, fieldname: str, fieldtype: type) -> bool:
+        """Checks if the specified field of worker configuration is correct
+        and matches the given type
+        """
+        if not fieldname in self._config:
+            s = 'У воркера {} с идентификатором "{}" нет поля с именем "{}"'
+            s = s.format(self._type, self._name, fieldname)
+            raise WorkerConfigurationException(s)
+
+        if not isinstance(self._config[fieldname], fieldtype):
+            s = 'У воркера {} с идентификатором "{}" поле "{}"'
+            s += ' неверного типа. Необходимый тип: "{}"'
+            s = s.format(self._type, self._name, fieldname, fieldtype)
+            raise WorkerConfigurationException(s)
+
+        return True
+
+
+class RoutedWorker(Worker):
+    def __init__(self, name: str, config: Dict):
+        """Abstract routed worker class with basic initialization
+        """
+        super().__init__(name, config)
+        self._type = 'routed'
+
+        self._is_correct_field('route', str)
+        self._route = config['route']
+
+
+class LoopedWorker(Worker):
+    def __init__(self, name: str, config: Dict):
+        """Abstract looped worker class with basic initialization
+        """
+        super().__init__(name, config)
+        self._type = 'looped'
+
+    def run(self) -> None:
+        """Work, that should be done each loop
+        """
+        raise NotImplementedError
