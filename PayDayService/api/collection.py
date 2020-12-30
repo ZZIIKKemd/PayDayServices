05e526083991ff00df2edb3dfd27c177f3f3f949
Andrey "zziikkemd" Zykov <zziikkemd@gmail.com>
2020-12-30 18:16:05 +0300

    Fixed types that can be stored in API collection

--- PayDayService/api/collection.py
+++ PayDayService/api/collection.py
@@ -1,9 +1,9 @@
 from typing import Dict
 
-from api.abstract import Api
+from api.type import ApiType
 
 
-class ApiCollection(Dict[str, Api]):
+class ApiCollection(Dict[str, ApiType]):
     def __init__(self):
         """Collection (basically a dictionary) of APIs by those identifiers
         """
